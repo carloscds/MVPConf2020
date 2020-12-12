57eeec7ac657e892660ec6ea02f49d361e173f78
Rafael Almeida <ralms@ralms.net>
2020-12-12 15:42:10 -0300

    Add code to project

--- Workshop.ContadoresDeEventos/Program.cs
+++ Workshop.ContadoresDeEventos/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Workshop.ContadoresDeEventos
 {
@@ -6,7 +9,41 @@ namespace Workshop.ContadoresDeEventos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var processId = Process.GetCurrentProcess().Id;// Environment.ProcessId;
+            Console.WriteLine($"Iniciando processo: {processId}");
+
+            using var db = new ExemploContexto();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            Console.WriteLine("Banco Criado..");
+
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+            {
+                db.Cursos.Add(new Curso { Descricao = "Workshop EF Core"});
+                db.SaveChanges();
+
+                var courses = db.Cursos.AsNoTracking().Where(p => p.Id > 0).ToArray();
+
+                Console.WriteLine($"Cursos: {courses.Length}");
+            }
+
+            Console.WriteLine("Finalizado..");
         }
+    }
+
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+
+    public class ExemploContexto : DbContext
+    {
+        public DbSet<Curso> Cursos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseSqlite("Data Source=contador-evento.db");
     }
 }
--- WorkshopEF.SplitQuery/Program.cs
+++ WorkshopEF.SplitQuery/Program.cs
@@ -1,17 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using WorkshopEF.Data;
 
-var connection = "Data Source=teste.db";
-var options = new DbContextOptionsBuilder<Contexto>()
-    .UseSqlite(connection)
-    .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
-
-var db = new Contexto(options.Options);
-SeedData.SetupBD(db);
-
-var grupos = db.Grupo.Include(i => i.Produto).AsSplitQuery();
-foreach (var g in grupos)
+namespace Workshop.SplitQuery
 {
-    Console.WriteLine($"{g.Id} - {g.Nome} - Produtos: {g.Produto.Count}");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db = new ExemploContexto();
+            Setup(db);
+            Console.WriteLine("Banco Criado..");
+
+            var departamentos = db.Departamentos.Include(p => p.Funcionarios).ToList();
+            foreach (var departamento in departamentos)
+            {
+                Console.WriteLine($"Departamento: {departamento.Descricao}");
+
+                foreach (var funcionario in departamento.Funcionarios)
+                {
+                    Console.WriteLine($"\t - {funcionario.Nome}");
+                }
+            }
+
+            Console.WriteLine("Finalizado..");
+        }
+
+        static void Setup(ExemploContexto db)
+        {
+            if (db.Database.EnsureCreated())
+            {
+                var departamentos = Enumerable.Range(1, 2)
+                .Select(p => new Departamento
+                {
+                    Descricao = "Departamento " + p,
+                    Funcionarios = Enumerable.Range(1, 10)
+                    .Select(x => new Funcionario
+                    {
+                        Nome = $"Funcionario {p}-{x}"
+                    }).ToList()
+                }).ToList();
+
+                db.Departamentos.AddRange(departamentos);
+
+                db.SaveChanges();
+            }
+        }
+
+
+    }
+
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Funcionario> Funcionarios { get; set; }
+    }
+
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public int DepartamentoId { get; set; }
+        public Departamento Departamento { get; set; }
+    }
+
+    public class ExemploContexto : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Departamento> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseSqlite("Data Source=split-query.db");
+    }
 }
