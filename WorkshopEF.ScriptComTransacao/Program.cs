f5374ceda21285bef3d446cc22919178563bad53
Rafael Almeida <ralms@ralms.net>
2020-12-12 15:11:53 -0300

    New projects

---
+++ WorkshopEF.ScriptComTransacao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorkshopEF.ScriptComTransacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+
+    public class ExemploContexto : DbContext
+    {
+        public DbSet<Curso> Cursos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseSqlite("Data Source=teste.db");
+    }
+}
